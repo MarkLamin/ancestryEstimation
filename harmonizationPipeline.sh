7e9376f6f2e89493ae9a71666cb83e87944daf2e
Mark Lamin <lamin022@agc05.agate.msi.umn.edu>
2025-07-14 12:13:07 -0500

    removed relationship cutoff

--- harmonizationPipeline.sh
+++ harmonizationPipeline.sh
@@ -108,7 +108,6 @@ Rscript $pathToRepo/snpNonBiAllelic.R $returnDirectory
 plink --bfile allData \
     --exclude nonBiAllelicSnps.txt \
     --allow-no-sex \
-    --rel-cutoff 0.185 \
     --make-bed \
     --out allDataBiAllelic
     
